2c197bbe8a432763dd504b870e21bf3b6a0a3ef4
Clare Jolly <clarejollyuk@gmail.com>
2018-11-25 22:22:47 +0000

    a real passing test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import { shallow, mount, render } from 'enzyme';
+
 import App from './App';
 import Notes from './components/notes/notes';
 
@@ -13,7 +22,24 @@ it('knows that 2 and 2 make 4', () => {
     expect(2 + 2).toBe(4);
   });
 
-// const container = shallow(<Notes {...props} />);
+// const container = shallow(<Notes/>);
 //   it('knows that 2 and 2 make 4', () => {
-//       expect(container.find('h1')).to.have.text('A New Day')
+//       expect(container.find('h1')).to.have.text('NodeNotes')
 //     });
+
+describe('Notes check', () => {
+  it('Page has this is a new note', () => {
+    const notes = shallow(<Notes />);
+    expect(notes.find('.notes').text()).toEqual('this is a new note');
+  });
+
+//   it('Page has this is a new note', () => {
+//     const notes = shallow(<Notes />);
+//     expect(notes.find('.notes-r').text()).toEqual('this is a new note');
+//   });
+//
+//   it('works with async/await and resolves', async () => {
+//   expect.assertions(1);
+//   expect(notes.find('.notes-r').text()).toEqual('this is a new note');
+// });
+});
